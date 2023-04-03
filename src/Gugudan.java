08edc16345789781f39ace962dd77beb1c34eaf3
RSH <RSH@DESKTOP-O6P42MM>
2023-04-04 00:06:28 +0900

    구구단 메소드 활용 출력

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,21 +1,24 @@
-import java.util.Scanner;
 public class Gugudan {
-
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		for(int i=0; i < result.length; i++) {
+			result[i] = times * (i + 1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i=0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	
 	public static void main(String[] args) {
-		// 8, 9단
-		System.out.println("구구단을 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("사용자가 입력한 값 : " + number);
+		for(int i=2; i < 10; i++) {
+			int[] result = calculate(i);
+			print(result);
+		}
 		
-		if(number < 2) {
-			System.out.println("값을 잘못 입력했습니다.");
-		} else if(number > 9) {
-			System.out.println("값을 잘못 입력했습니다.");
-		} else {
-		for(int i=1; i<10; i++) {
-			System.out.println(number * i);
 		}
-		}
-		}
-		}
+}
