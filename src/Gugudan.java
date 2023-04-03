283820edc51401382f568d439b2f749442f06a3b
RSH <RSH@DESKTOP-O6P42MM>
2023-04-03 23:41:34 +0900

    Gugudan_8dan

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,9 +1,16 @@
-
+import java.util.Scanner;
 public class Gugudan {
 
 	public static void main(String[] args) {
-		int a=1;
-
+		// 8, 9단
+		System.out.println("구구단을 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("사용자가 입력한 값 : " + number);
+		
+		for(int i=1; i<10; i++) {
+			System.out.println(number * i);
+		}
 	}
 
 }
