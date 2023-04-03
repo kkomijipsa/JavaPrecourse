84f1e4a478c277ef4dfe519703684e08811ce9a8
RSH <RSH@DESKTOP-O6P42MM>
2023-04-03 23:46:16 +0900

    8, 9단 if-for문

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -8,9 +8,14 @@ public class Gugudan {
 		int number = scanner.nextInt();
 		System.out.println("사용자가 입력한 값 : " + number);
 		
+		if(number < 2) {
+			System.out.println("값을 잘못 입력했습니다.");
+		} else if(number > 9) {
+			System.out.println("값을 잘못 입력했습니다.");
+		} else {
 		for(int i=1; i<10; i++) {
 			System.out.println(number * i);
 		}
-	}
-
-}
+		}
+		}
+		}
